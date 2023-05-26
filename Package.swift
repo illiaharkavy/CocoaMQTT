019104f360c5a11bb84c6188c3ae7f1fbd591f37
Illia Harkavy <illiaharkavy@gmail.com>
2023-05-26 15:10:55 +0200

    Naming fix

--- Package.swift
+++ Package.swift
@@ -4,28 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "CocoaMQTT",
+    name: "NetworkCocoaMQTT",
     platforms: [
         .macOS(.v10_15),
         .iOS(.v13),
         .tvOS(.v13)
     ],
     products: [
-        .library(name: "CocoaMQTT", targets: [ "CocoaMQTT" ]),
+        .library(name: "NetworkCocoaMQTT", targets: [ "NetworkCocoaMQTT" ]),
         .library(name: "CocoaMQTTWebSocket", targets: [ "CocoaMQTTWebSocket" ])
     ],
     targets: [
-        .target(name: "CocoaMQTT",
+        .target(name: "NetworkCocoaMQTT",
                 path: "Source",
                 exclude: ["CocoaMQTTWebSocket.swift"],
                 swiftSettings: [ .define("IS_SWIFT_PACKAGE")]),
         .target(name: "CocoaMQTTWebSocket",
-                dependencies: [ "CocoaMQTT" ],
+                dependencies: [ "NetworkCocoaMQTT" ],
                 path: "Source",
                 sources: ["CocoaMQTTWebSocket.swift"],
                 swiftSettings: [ .define("IS_SWIFT_PACKAGE")]),
         .testTarget(name: "CocoaMQTTTests",
-                    dependencies: [ "CocoaMQTT", "CocoaMQTTWebSocket" ],
+                    dependencies: [ "NetworkCocoaMQTT", "CocoaMQTTWebSocket" ],
                     path: "CocoaMQTTTests",
                     swiftSettings: [ .define("IS_SWIFT_PACKAGE")])
     ]
